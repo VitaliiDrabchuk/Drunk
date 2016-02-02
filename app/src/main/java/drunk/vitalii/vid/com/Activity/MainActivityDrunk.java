83c9b30e3c722ada640129673f38917cb670729c
Vitalii <vitalii.drabchuk@mobenga.com>
2016-02-02 19:40:20 +0200

    + Displaying contact list from phone; + Displaying toast message; + Change Activity after THE_BUTTON was clicked.

--- app/src/main/java/drunk/vitalii/vid/com/Activity/MainActivityDrunk.java
+++ app/src/main/java/drunk/vitalii/vid/com/Activity/MainActivityDrunk.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,72 +17,66 @@ import drunk.vitalii.vid.com.R;
 public class MainActivityDrunk extends Activity {
     public static final String SETUP_ACCOUNT = "drunk.vitalii.vid.com.Activity.SETUP_ACCOUNT";
 
-    private static final String VERSION = "version: ";
-    private static boolean newAccount = true;
-
-    private Button clickButton;
-    private TextView text;
-    private TextView versionLabel;
-    private boolean isTextVisible;
+    /* ---Service--- */
     private PackageInfo packageInfo;
+
+    /* ---UI--- */
+    private Button theButton;
+    private TextView versionLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_drunk);
 
-        versionLabel = (TextView) this.findViewById(R.id.versionInfoLabel);
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        if (packageInfo != null) {
-            versionLabel.setText(VERSION + packageInfo.versionName);
-        } else {
-            versionLabel.setVisibility(View.GONE);
-        }
-
-        clickButton = (Button) this.findViewById(R.id.clickBtn);
-        text = (TextView) this.findViewById(R.id.messageTxt);
-        text.setVisibility(View.GONE);
-
-//        applyListeners();
+        setupTheButton();
+        setupVersionLabel();
     }
 
-    private void applyListeners() {
-        final TextView textObj = this.text;
-
-        this.clickButton.setOnClickListener(new View.OnClickListener() {
+    /**
+     * Setup THE_BUTTON
+     */
+    private void setupTheButton() {
+        theButton = (Button) this.findViewById(R.id.theButton);
+        theButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                Log.i("RED_BUTTON", "is clicked");
-                if (newAccount) {
-                    Log.i("ACCOUNT", "is new");
-                    newAccount = false;
-                } else {
-                    Log.i("ACCOUNT", "exists");
-                    if (isTextVisible) {
-                        textObj.setVisibility(View.GONE);
-                    } else {
-                        textObj.setVisibility(View.VISIBLE);
-                    }
-                    isTextVisible = !isTextVisible;
-                }
-
+            public void onClick(View view) {
+                doEverythingFine(view);
             }
         });
     }
 
+
+    /**
+     * Displaying version of app.
+     * Receiving version from packageInfo and displaying it.
+     */
+    private void setupVersionLabel() {
+        versionLabel = (TextView) this.findViewById(R.id.versionInfoLabel);
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            if (packageInfo != null) {
+                versionLabel.setText(versionLabel.getText() + packageInfo.versionName);
+            } else {
+                versionLabel.setVisibility(View.GONE);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * The main action in app
+     */
     public void doEverythingFine(View view) {
         Intent intent = new Intent(this, SetupContacts.class);
         intent.putExtra(SETUP_ACCOUNT, true);
         startActivity(intent);
     }
 
-
+    /**
+     * Some overriding stuff
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/drunk/vitalii/vid/com/Activity/SetupContacts.java
+++ app/src/main/java/drunk/vitalii/vid/com/Activity/SetupContacts.java
@@ -2,33 +2,74 @@ package drunk.vitalii.vid.com.Activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import drunk.vitalii.vid.com.R;
 
 public class SetupContacts extends Activity {
+    /* ---Service--- */
+    String[] contactDetails = new String[]{ContactsContract.CommonDataKinds.Phone._ID,
+                                           ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                                           ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+    /* ---UI--- */
+    private TextView captionLabel;
+    private ListView contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(SetupContacts.class.toString(), " - in new Activity");
         setContentView(R.layout.activity_setup_contacts);
 
+        /* Greeting toast message */
+        Toast.makeText(SetupContacts.this, R.string.message_label, Toast.LENGTH_SHORT).show();
 
+        setupCaption();
+        setupContacts();
+    }
+
+    /**
+     * Receiving contacts from phone and displaying it
+     */
+    private void setupContacts() {
+        Uri contentUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        Cursor cursor = getContentResolver().query(contentUri, contactDetails, null, null, null);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            ArrayList<String> contactsFromPhone = new ArrayList<>();
+            String importedContact = "";
+
+            while (cursor.moveToNext()) {
+                importedContact = cursor.getString(1) + " (" + cursor.getString(2) + ")";
+//                Log.i("DATA", importedContact);
+                contactsFromPhone.add(importedContact);
+            }
+            contacts = (ListView)findViewById(R.id.contacts);
+            contacts.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsFromPhone));
+        }
+    }
+
+    /**
+     * Receives message from previous activity and displays it with captionLabel
+     */
+    private void setupCaption() {
         Intent intent = getIntent();
         boolean newAccount = intent.getBooleanExtra(MainActivityDrunk.SETUP_ACCOUNT, false);
         Log.i(SetupContacts.class.toString(), String.valueOf(newAccount));
 
-
-        TextView textView = (TextView)this.findViewById(R.id.newAccountMessage);
-        textView.setText("U R Drunk body! - " + String.valueOf(newAccount));
-
-//        setContentView(textView);
-
-
+        captionLabel = (TextView)this.findViewById(R.id.newAccountMessage);
+        captionLabel.setText(captionLabel.getText() + " " + String.valueOf(newAccount));
     }
 
     @Override
